63603fc1b7b7e7195b1cf10559a3dbd012767a40
Oscar Negrete <oscar.negrete09@gmail.com>
2023-12-06 14:12:42 +0000

    Updated append only streams statements

--- Section 7-Streams & CDC/02-append-only-streams.sql
+++ Section 7-Streams & CDC/02-append-only-streams.sql
@@ -1,7 +1,7 @@
 use role sysadmin;
 use database ecommerce_db;
  
-create  or replace schema streams_test;
+create or replace schema streams_test;
 
 --- Create a raw table to test the streams  ---
 CREATE OR REPLACE TABLE members_raw (
@@ -21,10 +21,10 @@ CREATE OR REPLACE TABLE members_prod (
 --- Create a append-only stream on the raw table :members_raw---
 CREATE OR REPLACE STREAM members_append_stream ON TABLE members_raw append_only=true;
 
---- Check the streams ---- 
+--- Check the streams (should be empty) ---- 
 select * from members_append_stream;
 
---- Check the stream offset ---- 
+--- Check the stream offset (should be 0) ---- 
 SELECT SYSTEM$STREAM_GET_TABLE_TIMESTAMP('members_append_stream') as members_table_st_offset;
 
 SELECT to_timestamp(SYSTEM$STREAM_GET_TABLE_TIMESTAMP('members_append_stream')) as members_table_st_offset;
@@ -41,47 +41,33 @@ VALUES
 --- Check the streams ---- 
 select * from members_append_stream;
 
---- Check the stream offset ---- 
-SELECT SYSTEM$STREAM_GET_TABLE_TIMESTAMP('members_std_stream') as members_table_st_offset;
+--- Check the stream offset  ---- 
+SELECT SYSTEM$STREAM_GET_TABLE_TIMESTAMP('members_append_stream') as members_table_st_offset;
 
 --- Query the streams data by ingesting the CDC streams data into the production table ---- 
-SELECT id, name, fee FROM members_std_stream WHERE METADATA$ACTION = 'INSERT';
+SELECT id, name, fee FROM members_append_stream;
 
 --- Consume the streams data by ingesting the CDC streams data into the production table : DML Operation ---- 
 INSERT INTO members_prod(id,name,fee) 
-SELECT id, name, fee FROM members_std_stream WHERE METADATA$ACTION = 'INSERT';
+SELECT id, name, fee FROM members_append_stream;
 
 --- Check the production table -----
 select * from members_prod;
 
+--- Check the streams -----
+select * from members_append_stream;
+
 --- Check the offset ---- 
-SELECT to_timestamp(SYSTEM$STREAM_GET_TABLE_TIMESTAMP('members_std_stream')) as members_table_st_offset;
+SELECT to_timestamp(SYSTEM$STREAM_GET_TABLE_TIMESTAMP('members_append_stream')) as members_table_st_offset;
 
+--- Update the raw table (SHOULD NOT work because it is only an APPEND stream) ----
+update members_raw set fee=10 where id=4;
 
---- Insert some more data into the raw table ---
-INSERT INTO members_raw (id,name,fee) VALUES (6,'sid',0),(7,'Billy',0),(8,'Katie',0);
+--- Check the streams (It does NOT capture the UPDATE because it is an APPEND stream) -----
+select * from members_append_stream;
 
---- Check the streams -----
-select * from members_std_stream;
+--- Delete the record from the raw table
+delete from members_raw where id=4;
 
---- Update the raw table ----
-update members_raw set fee=10 where id=7;
-
-
---- Check the streams -----
-select * from members_std_stream;
-
---- Consume the streams ---
-INSERT INTO members_prod(id,name,fee) SELECT id, name, fee FROM members_std_stream WHERE METADATA$ACTION = 'INSERT';
-
-
---- Lets update the raw table again ----
-update members_raw set fee=20 where id=7;
-
-
---- Consume both (inserts and updates) from the streams --- 
-merge into members_prod as mp 
-using (select id,name,fee from members_std_stream mstr where metadata$action='INSERT' ) as mstr
-on mp.id = mstr.id
-when matched then update set mp.fee = mstr.fee,mp.name = mstr.name
-when not matched then insert (id,name,fee) values (mstr.id,mstr.name,cast(mstr.fee as numeric));
+--- Check the streams (It does NOT capture the UPDATE because it is an APPEND stream) -----
+select * from members_append_stream;
